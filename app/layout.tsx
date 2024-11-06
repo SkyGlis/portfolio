0c021842f550083b7b2d3a4c7b37ce9d98764b38
Daniel Santos <56091623+SkyGlis@users.noreply.github.com>
2024-11-06 21:00:20 +0000

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { Montserrat } from 'next/font/google'
+
+const font = Montserrat({ 
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+        <body className={`${font.className} antialised`}>
+            {children}
+        </body>
     </html>
   );
 }
