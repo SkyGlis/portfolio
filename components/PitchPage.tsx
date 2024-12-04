c6194d0037b0d509eebad2dc42fdada3617ceb21
Daniel Santos <56091623+SkyGlis@users.noreply.github.com>
2024-12-04 15:14:03 +0000

    Change Video Handler

--- components/PitchPage.tsx
+++ components/PitchPage.tsx
@@ -1,9 +1,9 @@
+import { YouTubeEmbed } from '@next/third-parties/google'
+
 export default function PitchPage() {
   return (
-    <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 py-12 sm:py-24">
-      <video width="1280" height="780" controls preload="auto">
-        <source src="/pitch.mp4" type="video/mp4" />
-      </video>
+    <div className="max-w-7xl items-center mx-auto px-2 sm:px-6 lg:px-8 py-6 sm:py-24">
+      <YouTubeEmbed videoid='4o8BtFDZcLg' />
     </div>
   )
 }
